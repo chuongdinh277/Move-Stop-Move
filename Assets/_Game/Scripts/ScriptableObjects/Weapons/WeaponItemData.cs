6a53ac6a1ca33ac5bc3286cfe8e4167e14cd9fb4
Chuongdinh <quocchuong277@gmail.com>
2026-08-17 09:42:08 +0700

    +Add data Weapons

---
+++ Assets/_Game/Scripts/ScriptableObjects/Weapons/WeaponItemData.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+[System.Serializable]
+public class WeaponItemData
+{
+    public WeaponType weaponType;
+    public string weaponName;
+    public Weapon weaponPrefab;
+    public int cost;
+}
---
+++ Assets/_Game/Scripts/ScriptableObjects/Weapons/WeaponType.cs
@@ -0,0 +1,8 @@
+public enum WeaponType
+{
+    None = 0,
+    Hammer = 1,
+    Axe = 2,
+    Boomerang = 3,
+    Knife = 4
+}
